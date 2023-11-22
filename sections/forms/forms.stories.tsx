5cc4babc0c39247660c6d2c93508b811e5adb9a2
Chiril Gorbenco <chiril.gorbenco@student.upt.ro>
2023-11-22 15:03:15 +0200

    altu

--- sections/forms/forms.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { Meta, Story } from '@storybook/react';
-import Hero from './hero.section';
-
-export default {
-  title: 'Forms page section',
-} as Meta;
-
-export const FormsSection: Story = () => <Hero hildren={undefined} />;
